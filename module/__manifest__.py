372936a1fc61092effbb2d3adacec051fc34b4eb
mmbmha <mmbmha@me.com>
2018-01-10 10:48:01 +0300

    Update __manifest__.py

--- module/__manifest__.py
+++ module/__manifest__.py
@@ -3,11 +3,7 @@
     'version': '0.1',
     'category': 'Test',
     'summary': 'Test app',
-<<<<<<< HEAD:module/__manifest__.py
     # 'depends': ['sale_management'],
-=======
-    'depends': ['sale_management'],
->>>>>>> 4b4e64df40ed29a90aff65158f53577c3707d67c:dall/__manifest__.py
 
     'demo': [
     ],
