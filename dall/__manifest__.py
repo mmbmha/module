bd27f4be3412674a58441c416db34cc6c65eb5cf
mmbmha <mmbmha@me.com>
2018-01-10 06:54:12 +0300

    Initial commiting

--- dall/__manifest__.py
+++ dall/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Dall",
+    'name': "PO# Field",
 
     'summary': """
         The first App on odoo11 by Dall""",
@@ -16,19 +16,19 @@
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.21',
+    'version': '0.01',
 
     # any module necessary for this one to work correctly
     'depends': ['base','sale_management'],
 
     # always loaded
-    'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    # 'data': [
+    #     # 'security/ir.model.access.csv',
+    #     'views/views.xml',
+    #     'views/templates.xml',
+    # ],
+    # # only loaded in demonstration mode
+    # 'demo': [
+    #     'demo/demo.xml',
+    # ]
 }
